bcb6751a885f756a8eba25dfcf7df01664b6ae5f
D3VAR5H <dev103042@gmail.com>
2021-08-27 13:01:10 +0530

    Initial Setup for Twitter Clone React App

---
+++ twitter-web/src/Routes.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+
+const Routes = () => {
+	return <div></div>;
+};
+
+export default Routes;
---
+++ twitter-web/src/components/tweet-card/TweetCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TweetCard = () => {
+	return <div></div>;
+};
+
+export default TweetCard;
