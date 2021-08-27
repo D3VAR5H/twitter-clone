5cb834a362da22a77865a9b0b653c672e7e3f4a2
D3VAR5H <dev103042@gmail.com>
2021-08-27 18:55:01 +0530

    Profile, Home...
    feature | style (Profile.js, Home.js, profile.scss, home.scss): created pages for home and profile along with complete styling.
    refactor (*.scss): refactored to fix scrolling issue scrolling

--- twitter-web/src/pages/home/Home.js
+++ twitter-web/src/pages/home/Home.js
@@ -2,9 +2,27 @@ import React from "react";
 
 import TweetCard from "../../components/tweet-card/TweetCard";
 
+import "./home.scss";
+
 const Home = () => {
 	return (
-		<div>
+		<div className="home-container">
+			<div className="heading">Tweets</div>
+			<TweetCard />
+			<TweetCard />
+			<TweetCard />
+			<TweetCard />
+			<TweetCard />
+			<TweetCard />
+			<TweetCard />
+			<TweetCard />
+			<TweetCard />
+			<TweetCard />
+			<TweetCard />
+			<TweetCard />
+			<TweetCard />
+			<TweetCard />
+			<TweetCard />
 			<TweetCard />
 		</div>
 	);
--- twitter-web/src/pages/profile/Profile.js
+++ twitter-web/src/pages/profile/Profile.js
@@ -1,7 +1,33 @@
 import React from "react";
 
+import UserCard from "../../components/user-card/UserCard";
+import TweetCard from "../../components/tweet-card/TweetCard";
+
+import "./profile.scss";
+
 const Profile = () => {
-	return <div></div>;
+	return (
+		<div className="profile-container">
+			<div className="left-section">
+				<div className="wrapper">
+					<UserCard />
+				</div>
+				<div className="follow-btn">
+					<button>Follow</button>
+				</div>
+			</div>
+			<div className="right-section">
+				<TweetCard />
+				<TweetCard />
+				<TweetCard />
+				<TweetCard />
+				<TweetCard />
+				<TweetCard />
+				<TweetCard />
+				<TweetCard />
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
